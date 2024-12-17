06a089ed6c28c2572e596132df4fa5eaa9c7c7c7
Roee Chen <roee960001@gmail.com>
2024-12-17 14:58:27 +0200

    uploading first steps for building pokede

---
+++ pokemon.js
@@ -0,0 +1,5 @@
+const MAX_POKEMON = 151;
+const listWrapper = document.querySelector('.list-wrapper');
+const searchInput = document.querySelector('#search-input');
+const numberFIlter = document.querySelector('#number-filter');
+const nameFIlter = document.querySelector('#name');
