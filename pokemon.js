a6d0035bbf823cbd4a9ba0ed7c5b7a4b9d196d94
Roee Chen <roee960001@gmail.com>
2024-12-17 15:48:43 +0200

    fethcing pokemon data

--- pokemon.js
+++ pokemon.js
@@ -1,5 +1,51 @@
 const MAX_POKEMON = 151;
-const listWrapper = document.querySelector('.list-wrapper');
-const searchInput = document.querySelector('#search-input');
-const numberFIlter = document.querySelector('#number-filter');
-const nameFIlter = document.querySelector('#name');
+const listWrapper = document.querySelector(".list-wrapper");
+const searchInput = document.querySelector("#search-input");
+const numberFilter = document.querySelector("#number");
+const nameFilter = document.querySelector("#name");
+const notFoundMessage = document.querySelector("#not-found-message");
+
+let allPokemons = [];
+const pokemonIdPosition = 6;
+
+fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`)
+  .then((response) => response.json())
+  .then((data) => {
+    allPokemons = data.results;
+  });
+
+async function fetchPokemonDataBEforeRedirect(id) {
+  try {
+    const [pokemon, pokemonSpecies] = await Promise.all([
+      fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) =>
+        res.json()
+      ),
+      fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then((res) =>
+        res.json()
+      ),
+    ]);
+    return true;
+  } catch (error) {
+    console.error("Failed to fetch Pokemon data before redirect");
+  }
+}
+
+function displayPokemons(pokemon) {
+  listWrapper.innerHTML = "";
+  pokemon.forEach((pokemon) => {
+    const pokemonId = pokemon.url.split("/")[pokemonIdPosition];
+    const listItem = document.createElement("div");
+    listItem.className = "list-item";
+    listItem.innerHTML = `
+        <div class="number-wrap">
+            <p class="caption-fonts">#${pokemonID}</p>
+        </div>
+        <div class="img-wrap">
+            <img src="https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/official-artwork/${pokemonID}.svg" alt="${pokemon.name}" />
+        </div>
+        <div class="name-wrap">
+            <p class="body3-fonts">#${pokemon.name}</p>
+        </div>
+    `;
+  });
+}
